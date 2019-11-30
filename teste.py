4c78e16060381a133595c43f1c0817ef305e6b6c
FelipeMoreno <coutinhomoreno@gmail.com>
2019-11-30 14:40:22 -0300

    adicionando recurso de listar tarefas

---
+++ teste.py
@@ -0,0 +1,8 @@
+tafera = {
+    "titulo" : "comprar ovos",
+    "descrição": "ir no supermercado e comprar ovos"
+}
+
+if tafera["titulo"] == "comprar ovos":
+    tafera["estado"] = "finalizado"
+    print(tafera["estado"])
